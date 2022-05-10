155fc39f8eacde1ec358adf61f707816c9cd3f0a
jorisguffens <joris.guffens@outlook.com>
2022-05-10 18:43:25 +0000

    Deploying to javadocs from @ GufliMC/BrickI18n@bd6c27e90531e68ba58b826b668c0526c4425734 🚀

---
+++ minestom/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.guflimc.brick.i18n.minestom"},{"l":"com.guflimc.brick.i18n.minestom.namespace"}];updateSearchResults();
